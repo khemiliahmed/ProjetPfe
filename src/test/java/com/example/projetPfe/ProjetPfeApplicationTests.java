18470ccc091cfa115cbcc5b4d17649bf92ba2266
ahmed <khemilihmd@gmail.com>
2025-03-09 23:53:56 +0100

    first commit

---
+++ src/test/java/com/example/projetPfe/ProjetPfeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetPfe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetPfeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
